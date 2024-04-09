362d4d63d61eddbbf877594942aeb4bd516bb2af
zzangse <zzangse1014@gmail.com>
2024-04-09 20:32:56 +0900

    ✨ 버튼클릭 이벤트 설정

---
+++ app/src/main/java/com/zzangse/attendance_check/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.zzangse.attendance_check;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.zzangse.attendance_check.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+    ActivityLoginBinding loginBinding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initView();
+        onClickBtn();
+    }
+
+    private void initView() {
+        loginBinding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(loginBinding.getRoot());
+    }
+
+    private void onClickBtn() {
+        loginBinding.btnLogin.setOnClickListener(v->{
+            Toast.makeText(this,"test",Toast.LENGTH_SHORT).show();
+        });
+    }
+}
