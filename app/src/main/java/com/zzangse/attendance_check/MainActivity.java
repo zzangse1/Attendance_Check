48a99fe68c3ed7b463acea738322d2021c67f7ad
sehoon <zzangse1014@gmail.com>
2024-04-13 17:05:19 +0900

    ✨ 프래그먼트 구현
    프래그먼트 4개 보여주기

--- app/src/main/java/com/zzangse/attendance_check/MainActivity.java
+++ app/src/main/java/com/zzangse/attendance_check/MainActivity.java
@@ -1,24 +1,44 @@
 package com.zzangse.attendance_check;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.navigation.NavigationBarView;
 import com.zzangse.attendance_check.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding mainBinding;
-    private NavController navController;
+    private CheckFragment checkFragment;
+    private ChartFragment chartFragment;
+    private EditFragment editFragment;
+    private MoreFragment moreFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initView();
-        setJetPackNavigation();
+
+        checkFragment = new CheckFragment();
+        chartFragment = new ChartFragment();
+        editFragment = new EditFragment();
+        moreFragment = new MoreFragment();
+
+        replace(new CheckFragment());
+        BottomNavSelect bottomNavSelect = new BottomNavSelect();
+        mainBinding.bottomNav.setOnItemSelectedListener(bottomNavSelect);
+
     }
 
     private void initView() {
@@ -26,16 +46,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(mainBinding.getRoot());
     }
 
-    private void setJetPackNavigation() {
-        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-    }
+    class BottomNavSelect implements NavigationBarView.OnItemSelectedListener{
 
-    @Override
-    public void onBackPressed() {
-        if (!navController.popBackStack()) {
-            super.onBackPressed();
-
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            if (item.getItemId() == R.id.menu_check) {
+                replace(checkFragment);
+            } else if (item.getItemId() == R.id.menu_chart) {
+                replace(chartFragment);
+            }else if (item.getItemId() == R.id.menu_edit) {
+                replace(editFragment);
+            } else if (item.getItemId() == R.id.menu_more) {
+                replace(moreFragment);
+            }
+            return true;
         }
-
+    }
+    private void replace(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.nav_host_fragment,fragment);
+        transaction.commit();
     }
 }
