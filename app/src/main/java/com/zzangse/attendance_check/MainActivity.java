d98d6640bfa229390fefccb55034bba2fd6b7dc4
zzangse <zzangse1014@gmail.com>
2024-04-13 13:54:09 +0900

    ✨ 프래그먼트 관리

--- app/src/main/java/com/zzangse/attendance_check/MainActivity.java
+++ app/src/main/java/com/zzangse/attendance_check/MainActivity.java
@@ -3,19 +3,39 @@ package com.zzangse.attendance_check;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.zzangse.attendance_check.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    ActivityMainBinding mainBinding;
+    private ActivityMainBinding mainBinding;
+    private NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initView();
+        setJetPackNavigation();
     }
 
     private void initView() {
         mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mainBinding.getRoot());
+    }
+
+    private void setJetPackNavigation() {
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!navController.popBackStack()) {
+            super.onBackPressed();
+
+        }
+
     }
 }
