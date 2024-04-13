c90d66405e5c48d786c5e5020f3c298b7597e2e9
zzangse <zzangse1014@gmail.com>
2024-04-13 13:54:00 +0900

    ✨ 프래그먼트.java 파일 생성

---
+++ app/src/main/java/com/zzangse/attendance_check/EditFragment.java
@@ -0,0 +1,30 @@
+package com.zzangse.attendance_check;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link EditFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class EditFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit, container, false);
+    }
+}
