2e9373ca2e9a874fe89590adbff6025225378b18
zzangse <zzangse1014@gmail.com>
2024-04-17 21:08:55 +0900

    ✨ 로그인 기능 구현

--- app/src/main/java/com/zzangse/attendance_check/activity/LoginActivity.java
+++ app/src/main/java/com/zzangse/attendance_check/activity/LoginActivity.java
@@ -5,21 +5,36 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.zzangse.attendance_check.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
-    ActivityLoginBinding loginBinding;
+    private ActivityLoginBinding loginBinding;
+    private FirebaseAuth mFirebaseAuth;
+    private DatabaseReference mDatabaseRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initView();
-        onClickBtn();
+        initFirebase();
+        onClickLogin();
         onClickSignUp();
+    }
+
+    private void initFirebase() {
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
     }
 
     private void initView() {
@@ -27,9 +42,24 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(loginBinding.getRoot());
     }
 
-    private void onClickBtn() {
+
+    private void onClickLogin() {
         loginBinding.btnLogin.setOnClickListener(v->{
-            Toast.makeText(this,"test",Toast.LENGTH_SHORT).show();
+            String id = loginBinding.etEmailId.getText().toString();
+            String password = loginBinding.etPassword.getText().toString();
+            mFirebaseAuth.signInWithEmailAndPassword(id, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        // 로그인 성공
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "false", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         });
     }
 
