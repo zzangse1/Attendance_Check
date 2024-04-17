55271040c0b5a8c6270cdd20c194e5fcdd3aadc2
zzangse <zzangse1014@gmail.com>
2024-04-17 21:09:30 +0900

    ✨ 계정 생성자 클래스 생성

---
+++ app/src/main/java/com/zzangse/attendance_check/UserAccount.java
@@ -0,0 +1,72 @@
+package com.zzangse.attendance_check;
+
+public class UserAccount {
+    String idToken, id, password, nickName, name, birth, sex, number;
+
+    public UserAccount() {
+    }
+
+    public String getIdToken() {
+        return idToken;
+    }
+
+    public void setIdToken(String idToken) {
+        this.idToken = idToken;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
