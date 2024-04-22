1762683fc6b39b244c2b9d54f02635b36095317c
zzangse1 <zzangse1014@gmail.com>
2024-04-22 09:14:20 +0900

    ✅ 저장 클릭 테스트 구현
    저장 버튼 누르면 토스트 메시지 출력

--- app/src/main/java/com/zzangse/attendance_check/activity/GroupAddActivity.java
+++ app/src/main/java/com/zzangse/attendance_check/activity/GroupAddActivity.java
@@ -1,14 +1,10 @@
 package com.zzangse.attendance_check.activity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.zzangse.attendance_check.R;
 import com.zzangse.attendance_check.databinding.ActivityGroupAddBinding;
 
 public class GroupAddActivity extends AppCompatActivity {
@@ -17,10 +13,18 @@ public class GroupAddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initView();
+        onClickBtn();
     }
 
     private void initView() {
         activityGroupAddBinding = ActivityGroupAddBinding.inflate(getLayoutInflater());
         setContentView(activityGroupAddBinding.getRoot());
+    }
+
+    private void onClickBtn() {
+        activityGroupAddBinding.btnOk.setOnClickListener(v->{
+            Toast.makeText(this,"생성되었습니다.",Toast.LENGTH_SHORT).show();
+            onBackPressed();
+        });
     }
 }
