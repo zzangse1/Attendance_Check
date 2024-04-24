f40db07135c07ebdb1f6f1e8777257446be0ca53
zzangse1 <zzangse1014@gmail.com>
2024-04-24 17:14:24 +0900

    ✨ 회원가입 통신코드 구현

---
+++ app/src/main/java/com/zzangse/attendance_check/request/SignupRequest.java
@@ -0,0 +1,32 @@
+package com.zzangse.attendance_check.request;
+
+import androidx.annotation.Nullable;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignupRequest  extends StringRequest {
+    private final static String URL ="http://zzangse.dothome.co.kr/Register.php";
+    private Map<String,String> map;
+
+
+    public SignupRequest(String userID, String userPassword, String userName, int userAge,Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("userID", userID);
+        map.put("userPassword", userPassword);
+        map.put("userName", userName);
+        map.put("userAge", userAge+"");
+    }
+
+    @Nullable
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
