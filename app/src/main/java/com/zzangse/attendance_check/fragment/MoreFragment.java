daf71d754ec0b6c8215a18ab76d7590a7d5b3c70
zzangse1 <zzangse1014@gmail.com>
2024-04-25 21:10:38 +0900

    ✨ userID를 메인액티비티로부터 받는 코드

--- app/src/main/java/com/zzangse/attendance_check/fragment/MoreFragment.java
+++ app/src/main/java/com/zzangse/attendance_check/fragment/MoreFragment.java
@@ -1,17 +1,20 @@
 package com.zzangse.attendance_check.fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.zzangse.attendance_check.R;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.zzangse.attendance_check.databinding.FragmentMoreBinding;
 
 
 public class MoreFragment extends Fragment {
+    private FragmentMoreBinding binding;
+    private String userID;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +23,26 @@ public class MoreFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false);
+        binding = FragmentMoreBinding.inflate(inflater);
+        getArgs();
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // 구현
+        setTextView();
+    }
+
+    private void getArgs() {
+        Bundle args = getArguments();
+        if (args != null) {
+            userID = args.getString("userID");
+        }
+    }
+
+    private void setTextView() {
+        binding.tvId.setText(userID);
     }
 }
