32fabb8a98fa9e44794788b2e0086df80647b2ed
sehoon <zzangse1014@gmail.com>
2024-04-27 17:44:20 +0900

    ✨ datePikcer 구현

--- app/src/main/java/com/zzangse/attendance_check/fragment/CheckFragment.java
+++ app/src/main/java/com/zzangse/attendance_check/fragment/CheckFragment.java
@@ -1,25 +1,34 @@
 package com.zzangse.attendance_check.fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.zzangse.attendance_check.databinding.FragmentCheckBinding;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class CheckFragment extends Fragment {
     private FragmentCheckBinding checkBinding;
-
+    private Calendar calendar;
+    private Date date = new Date();
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yy년 M월 dd일");
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container,
                              @NonNull Bundle savedInstanceState) {
@@ -27,21 +36,42 @@ public class CheckFragment extends Fragment {
         return checkBinding.getRoot();
     }
 
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-      //  test();
+        setDate();
+        onClickDate();
     }
 
-//    private void test() {
-//        Bundle bundle = getArguments();
-//        if (bundle != null) {
-//            String id = bundle.getString("userID");
-//            String ps = bundle.getString("userPW");
-//            String nickName = bundle.getString("nickName");
-//            checkBinding.tvId.setText(id);
-//            checkBinding.tvPw.setText(ps);
-//            checkBinding.tvNickname.setText(nickName);
-//        }
-//    }
+    private void setDate() {
+        simpleDateFormat = new SimpleDateFormat("yy년 M월 dd일");
+        String today = simpleDateFormat.format(date);
+        checkBinding.tvDate.setText(today);
+    }
+
+    private void onClickDate() {
+        calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        // today
+        Long today = MaterialDatePicker.todayInUtcMilliseconds();
+        checkBinding.tvDate.setOnClickListener(v -> {
+            MaterialDatePicker datePicker = MaterialDatePicker.Builder.datePicker()
+                    .setTitleText("")
+                    .setSelection(today).build();
+
+            datePicker.show(getActivity().getSupportFragmentManager(), "DATE_PICKER");
+
+            datePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
+                @Override
+                public void onPositiveButtonClick(Long selection) {
+                    simpleDateFormat = new SimpleDateFormat("yy년 M월 d일");
+                    date.setTime(selection);
+                    String dateString = simpleDateFormat.format(date);
+                    checkBinding.tvDate.setText(dateString);
+
+                }
+            });
+        });
+
+    }
 }
