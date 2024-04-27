f52253eef164e284e78e7c1886fb8e1e5082ff0a
sehoon <zzangse1014@gmail.com>
2024-04-27 17:46:53 +0900

    ✨ groupName 클래스 파일

---
+++ app/src/main/java/com/zzangse/attendance_check/GroupName.java
@@ -0,0 +1,17 @@
+package com.zzangse.attendance_check;
+
+public class GroupName {
+    private String groupName;
+
+    public GroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+}
