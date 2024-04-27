e70d55b32add9767114a8e54796b97ece66069a5
sehoon <zzangse1014@gmail.com>
2024-04-27 17:46:16 +0900

    ✅ json 읽기 테스트 코드

--- app/src/main/java/com/zzangse/attendance_check/fragment/EditFragment.java
+++ app/src/main/java/com/zzangse/attendance_check/fragment/EditFragment.java
@@ -20,6 +20,7 @@ import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.zzangse.attendance_check.GroupName;
 import com.zzangse.attendance_check.R;
 import com.zzangse.attendance_check.activity.SettingActivity;
 import com.zzangse.attendance_check.databinding.FragmentEditBinding;
@@ -28,10 +29,25 @@ import com.zzangse.attendance_check.request.GroupRequest;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class EditFragment extends Fragment {
     private FragmentEditBinding binding;
     private String userID;
-
+    ArrayList<GroupName> groupNames = new ArrayList<>();
+    
+    // 그룹이름 공백 제한 규칙 만들기
+    // http://webs.co.kr/index.php?document_srl=3312513&mid=nodejs
+    // https://lcw126.tistory.com/102
+    // https://velog.io/@thwjd9393/%EC%82%AC%EC%9A%A9%EC%9E%90%EC%9D%98-%EC%9A%94%EC%B2%AD%EA%B7%9C%EC%95%BDwith-AndroidGETPOSTDB%EC%A0%80%EC%9E%A5
+    // https://webnautes.tistory.com/1189#google_vignette
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +74,7 @@ public class EditFragment extends Fragment {
         // 구현
         onClickGroupAdd();
         onClickTest();
+        loadData();
     }
 
     private void onClickGroupAdd() {
@@ -142,5 +159,54 @@ public class EditFragment extends Fragment {
         } else {
             Log.d("EditFragment", "getActivity == null");
         }
+    }
+
+    private void loadData() {
+        new Thread(){
+            @Override
+            public void run() {
+                String serverAdress = "http://zzangse.dothome.co.kr/LoadGroupName.php";
+                try {
+                    URL url = new URL(serverAdress);
+                    HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+
+                    connection.setRequestMethod("GET");
+                    connection.setDoInput(true);
+                    connection.setUseCaches(false);
+
+                    InputStream is = connection.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(is);
+                    BufferedReader reader = new BufferedReader(isr);
+                    StringBuffer buffer = new StringBuffer();
+                    while (true) {
+                        String line = reader.readLine();
+                        if(line==null) break;
+                        buffer.append(line + "\n");
+                        Log.d("TAG", line);
+                        binding.tvTestJson.setText(line);
+                    }
+                    String[] rows = buffer.toString().split(",");
+                    Log.d("TAG", rows[0]);
+                    Log.i("TAG", rows.length + " ");
+                    for (String str : rows) {
+                        String[] datas = str.split(",");
+                        if (datas.length!=2)continue;
+
+                        int no = Integer.parseInt(datas[0]);
+                        String groupName = datas[0];
+                        groupNames.add(new GroupName(groupName));
+                        binding.tvTestJson.setText(groupName);
+                    }
+//                    runOnUiThread(()->{
+//                        adapter.notifyDataSetChanged(); //리사이클러뷰에 화면 갱신
+//                    });
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                super.run();
+            }
+        }.start();
     }
 }
