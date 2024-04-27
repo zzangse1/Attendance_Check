0bf081bea01a18bb71c1dd991a085ad953d62276
zzangse1 <zzangse1014@gmail.com>
2024-04-27 13:44:56 +0900

    ✨ 그룹네임 규칙 구현

--- app/src/main/java/com/zzangse/attendance_check/fragment/EditFragment.java
+++ app/src/main/java/com/zzangse/attendance_check/fragment/EditFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -18,6 +19,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.zzangse.attendance_check.R;
 import com.zzangse.attendance_check.activity.SettingActivity;
 import com.zzangse.attendance_check.databinding.FragmentEditBinding;
@@ -77,6 +79,8 @@ public class EditFragment extends Fragment {
         View dialogView = inflater.inflate(R.layout.dialog_view, null);
         Button btnOK = dialogView.findViewById(R.id.btn_ok);
         Button btnCancel = dialogView.findViewById(R.id.btn_cancel);
+        TextInputLayout textInputLayout = dialogView.findViewById(R.id.et_group_name_layout);
+        TextView textView = dialogView.findViewById(R.id.tv_group_name_error);
         TextInputEditText textInputEditText = dialogView.findViewById(R.id.et_group_name);
 
         AlertDialog dialog = builder.setView(dialogView)
@@ -87,16 +91,33 @@ public class EditFragment extends Fragment {
             Toast.makeText(getActivity(), "cancel", Toast.LENGTH_SHORT).show();
             dialog.dismiss();
         });
+
         btnOK.setOnClickListener(v -> {
             Toast.makeText(getActivity(), "ok", Toast.LENGTH_SHORT).show();
             String groupName = textInputEditText.getText().toString();
-            setGroupDB(userID,groupName);
-            binding.tvTest.setText(userID+", "+groupName);
-            dialog.dismiss();
+            if (checkGroupName(groupName)) {
+                setGroupDB(userID, groupName);
+                binding.tvTest.setText(userID + ", " + groupName);
+                dialog.dismiss();
+            } else {
+                textInputLayout.setBoxStrokeColor(getResources().getColor(R.color.red));
+                textView.setVisibility(View.VISIBLE);
+            }
+
         });
         dialog.show();
     }
 
+    private boolean checkGroupName(String groupName) {
+        boolean isCheck = 2 <= groupName.length() && groupName.length() <= 10;
+        if (isCheck) {
+            Toast.makeText(getActivity(), "db입력 허용", Toast.LENGTH_SHORT).show();
+            return true;
+        } else {
+            Toast.makeText(getActivity(), "db입력 불허", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
     private void setGroupDB(String userID, String groupName) {
         Response.Listener<String> listener = new Response.Listener<String>() {
             @Override
