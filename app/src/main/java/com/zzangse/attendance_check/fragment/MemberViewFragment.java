12eff114b67084c27dd51621f0f936b54144924a
zzangse1 <zzangse1014@gmail.com>
2024-05-02 21:27:34 +0900

    ✨ 3개 기능 구현
    뒤로가기
    액티비티에서 그룹이름 받아오기
    툴바 추가버튼 누르면 프래그먼트 이동

--- app/src/main/java/com/zzangse/attendance_check/fragment/MemberViewFragment.java
+++ app/src/main/java/com/zzangse/attendance_check/fragment/MemberViewFragment.java
@@ -2,22 +2,27 @@ package com.zzangse.attendance_check.fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import com.zzangse.attendance_check.R;
 import com.zzangse.attendance_check.activity.SettingActivity;
 import com.zzangse.attendance_check.databinding.FragmentMemberViewBinding;
 
 public class MemberViewFragment extends Fragment {
-    private FragmentMemberViewBinding binding;
 
-    public static MemberViewFragment newInstance() {
+    private FragmentMemberViewBinding binding;
+    public static MemberViewFragment newInstance(String groupName) {
         MemberViewFragment fragment = new MemberViewFragment();
         Bundle args = new Bundle();
+        args.putString("groupName", groupName);
         fragment.setArguments(args);
         return fragment;
     }
@@ -35,13 +40,48 @@ public class MemberViewFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         // 구현
         test();
+        onClickBack();
+        setGroupName();
+        onClickBtnAdd();
+    }
+    
+    private void onClickBtnAdd() {
+        binding.toolbarMemberView.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.ib_add) {
+                    Toast.makeText(getActivity(), "ib_add", Toast.LENGTH_SHORT).show();
+                    test2();
+                    return true;
+                }
+     
+                return false;
+            }
+            
+        });
+    }
+    private void setGroupName() {
+        String groupName = getArguments().getString("groupName");
+        binding.tvGroupName.setText(groupName);
+    }
+    private void onClickBack() {
+        binding.toolbarMemberView.setNavigationOnClickListener(v -> {
+            getActivity().onBackPressed();
+        });
     }
 
+    private void test2() {
+        if (getActivity() instanceof SettingActivity) {
+            ((SettingActivity) getActivity()).onFragmentChanged(1);
+        }
+    }
     private void test() {
-        binding.btnViewTest.setOnClickListener(v->{
+        binding.btnViewTest.setOnClickListener(v -> {
             if (getActivity() instanceof SettingActivity) {
-                ((SettingActivity)getActivity()).onFragmentChanged(0);
+                ((SettingActivity) getActivity()).onFragmentChanged(0);
             }
         });
     }
+
 }
