378b2fee3b3bbec3be3b86b03a6beaa0ea1da7d0
zzangse1 <zzangse1014@gmail.com>
2024-05-02 21:29:10 +0900

    ✨ groupName 받아오기
    편집프래그먼트에서 groupName을 보낸 것을 받아오는 코드

--- app/src/main/java/com/zzangse/attendance_check/activity/SettingActivity.java
+++ app/src/main/java/com/zzangse/attendance_check/activity/SettingActivity.java
@@ -1,6 +1,8 @@
 package com.zzangse.attendance_check.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -8,34 +10,44 @@ import androidx.fragment.app.FragmentManager;
 
 import com.zzangse.attendance_check.R;
 import com.zzangse.attendance_check.databinding.ActivitySettingBinding;
+import com.zzangse.attendance_check.fragment.MemberAddFragment;
 import com.zzangse.attendance_check.fragment.MemberInfoFragment;
 import com.zzangse.attendance_check.fragment.MemberModifyFragment;
 import com.zzangse.attendance_check.fragment.MemberViewFragment;
 
 public class SettingActivity extends AppCompatActivity {
-    private ActivitySettingBinding activitySettingBinding;
+    private ActivitySettingBinding binding;
     private FragmentManager fragmentManager; // 참조할 수 있는 변수 선언
     private MemberViewFragment viewFragment;
+    private MemberAddFragment addFragment;
     private MemberInfoFragment infoFragment;
     private MemberModifyFragment modifyFragment;
+    private String groupName = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initView();
         initFragment();
-
     }
     private void initView() {
-        activitySettingBinding = ActivitySettingBinding.inflate(getLayoutInflater());
-        setContentView(activitySettingBinding.getRoot());
+        binding = ActivitySettingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 
     private void initFragment() {
+        getGroupName();
         fragmentManager = getSupportFragmentManager();
-        viewFragment = MemberViewFragment.newInstance();
+        viewFragment = MemberViewFragment.newInstance(groupName);
+        addFragment = MemberAddFragment.newInstance();
         infoFragment = MemberInfoFragment.newInstance();
         modifyFragment = MemberModifyFragment.newInstance();
         fragmentManager.beginTransaction().add(R.id.fragment_setting, viewFragment).commit();
+    }
+
+    private void getGroupName() {
+        Intent intent = getIntent();
+        groupName = intent.getStringExtra("groupName");
+        Log.d("groupName", groupName);
     }
 
 
@@ -46,6 +58,9 @@ public class SettingActivity extends AppCompatActivity {
             selectedFragment= MemberInfoFragment.newInstance();
             tag = "info_fragment";
         } else if (index == 1) {
+            selectedFragment = MemberAddFragment.newInstance();
+            tag = "add_fragment";
+        } else if (index == 2) {
             selectedFragment = MemberModifyFragment.newInstance();
             tag = "modify_fragment";
         }
