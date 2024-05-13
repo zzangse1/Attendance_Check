d6b7fafc476e8afcdd43feeb87f9f714d73d2737
zzangse1 <zzangse1014@gmail.com>
2024-05-13 16:00:54 +0900

    🗃 db멤버추가 클래스

---
+++ app/src/main/java/com/zzangse/attendance_check/request/AddMemberRequest.java
@@ -0,0 +1,36 @@
+package com.zzangse.attendance_check.request;
+
+import androidx.annotation.Nullable;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddMemberRequest extends StringRequest {
+    private final static String URL ="http://zzangse.dothome.co.kr/MemberInsert.php";
+    private Map<String,String> map;
+
+
+    public AddMemberRequest(String userID, String groupName,String name, String number, String number2,
+                            String address, String memo,Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("userID", userID);
+        map.put("groupName", groupName);
+        map.put("name", name);
+        map.put("number", number);
+        map.put("number2", number2);
+        map.put("address", address);
+        map.put("memo", memo);
+    }
+
+    @Nullable
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
