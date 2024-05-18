de44dbb6c968bec852ffa60d397263edee785195
sehoon <zzangse1014@gmail.com>
2024-05-18 14:02:41 +0900

    🗃 멤버 수정 리퀘스트코드

---
+++ app/src/main/java/com/zzangse/attendance_check/request/ModifyMemberRequest.java
@@ -0,0 +1,35 @@
+package com.zzangse.attendance_check.request;
+
+import androidx.annotation.Nullable;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ModifyMemberRequest extends StringRequest {
+    private final static String URL = "http://zzangse.store/modify_member.php";
+    private Map<String, String> map;
+
+    public ModifyMemberRequest(int priNum, String modifyGroupName, String modifyName, String modifyNumber,
+                               String modifyNumber2, String modifyAddress, String modifyMemo, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("priNum", String.valueOf(priNum));
+        map.put("modifyGroupName", modifyGroupName);
+        map.put("modifyName", modifyName);
+        map.put("modifyNumber", modifyNumber);
+        map.put("modifyNumber2", modifyNumber2);
+        map.put("modifyAddress", modifyAddress);
+        map.put("modifyMemo", modifyMemo);
+    }
+
+    @Nullable
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
