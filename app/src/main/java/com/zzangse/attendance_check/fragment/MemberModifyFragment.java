762e1e52afe2916e339de6958e2bd5d963f7e3a5
sehoon <zzangse1014@gmail.com>
2024-05-18 14:02:07 +0900

    ✨ 멤버정보 수정 구현

--- app/src/main/java/com/zzangse/attendance_check/fragment/MemberModifyFragment.java
+++ app/src/main/java/com/zzangse/attendance_check/fragment/MemberModifyFragment.java
@@ -3,6 +3,7 @@ package com.zzangse.attendance_check.fragment;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +13,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 import com.zzangse.attendance_check.activity.SettingActivity;
 import com.zzangse.attendance_check.data.MemberInfo;
 import com.zzangse.attendance_check.databinding.FragmentMemberModifyBinding;
 import com.zzangse.attendance_check.request.LoadMemberViewRequest;
+import com.zzangse.attendance_check.request.ModifyMemberRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -25,6 +30,7 @@ public class MemberModifyFragment extends Fragment {
     private FragmentMemberModifyBinding binding;
     private int priNum;
     private MemberInfo memberInfo;
+    private String modifyGroupName, modifyName, modifyNumber, modifyNumber2, modifyAddress, modifyMemo;
 
     public static MemberModifyFragment newInstance(int priNum) {
         MemberModifyFragment fragment = new MemberModifyFragment();
@@ -56,6 +62,18 @@ public class MemberModifyFragment extends Fragment {
         onClickBack();
         loadMemberData();
         textWatcher();
+
+        //  test();
+    }
+
+    private void getMemberInfo() {
+        binding.tvPri.setText(priNum + "");
+        String newGroupName = binding.etModifyGroupName.getText().toString();
+        // newName = binding.etModifyPersonName.getText().toString();
+        String newPhoneNumber = binding.etModifyPersonNumber.getText().toString();
+        String newPhoneNumber2 = binding.etModifyPersonNumber2.getText().toString();
+        String newAddress = binding.etModifyPersonAddress.getText().toString();
+        String newMemo = binding.etModifyPersonMemo.getText().toString();
     }
 
     private void onClickBack() {
@@ -124,14 +142,57 @@ public class MemberModifyFragment extends Fragment {
 
     private void onClickSaveBtn() {
         binding.btnSave.setOnClickListener(v -> {
+            updateMemberInfo();
             Toast.makeText(getActivity(), "저장되었습니다.", Toast.LENGTH_SHORT).show();
             moveToMemberViewFragment();
         });
     }
 
+    private void updateMemberInfo() {
+        modifyGroupName = binding.etModifyGroupName.getText().toString();
+        modifyName = binding.etModifyPersonName.getText().toString();
+        modifyNumber = binding.etModifyPersonNumber.getText().toString();
+        modifyNumber2 = binding.etModifyPersonNumber2.getText().toString();
+        modifyAddress = binding.etModifyPersonAddress.getText().toString();
+        modifyMemo = binding.etModifyPersonMemo.getText().toString();
+        ModifyMemberRequest request = new ModifyMemberRequest(priNum, modifyGroupName, modifyName,
+                modifyNumber, modifyNumber2, modifyAddress, modifyMemo, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                Log.d("name", s);
+            }
+        });
+        if (getActivity() != null) {
+            RequestQueue queue = Volley.newRequestQueue(getActivity());
+            queue.add(request);
+        }
+    }
+
+//    private void test() {
+//        getParentFragmentManager().setFragmentResultListener("memberInfo", this, new FragmentResultListener() {
+//            @Override
+//            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+//                String groupName = result.getString("groupName");
+//                String infoName = result.getString("infoName");
+//                String infoPhoneNumber = result.getString("infoPhoneNumber");
+//                String infoPhoneNumber2 = result.getString("infoPhoneNumber2");
+//                String infoAddress = result.getString("infoAddress");
+//                String infoMemo = result.getString("infoMemo");
+//                Log.d("AAA", groupName);
+//                Log.d("AAA", infoName);
+//                Log.d("AAA", infoPhoneNumber);
+//                Log.d("AAA", infoPhoneNumber2);
+//                Log.d("AAA", infoAddress);
+//                Log.d("AAA", infoMemo);
+//            }
+//        });
+//    }
+
     private void moveToMemberViewFragment() {
         if (getActivity() instanceof SettingActivity) {
+            Log.d("modify -> view", priNum + "");
             ((SettingActivity) getActivity()).onFragmentChanged(3, 0);
+
         }
     }
 }
