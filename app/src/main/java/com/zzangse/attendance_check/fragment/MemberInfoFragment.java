fef3afe7578591a100695f4ffa78446ca2c347c1
zzangse1 <zzangse1014@gmail.com>
2024-05-22 18:58:19 +0900

    🔥 onViewCreate 제거

--- app/src/main/java/com/zzangse/attendance_check/fragment/MemberInfoFragment.java
+++ app/src/main/java/com/zzangse/attendance_check/fragment/MemberInfoFragment.java
@@ -38,12 +38,6 @@ public class MemberInfoFragment extends Fragment {
     }
 
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
