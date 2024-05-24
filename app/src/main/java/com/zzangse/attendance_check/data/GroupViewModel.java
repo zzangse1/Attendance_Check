da129e01effd6bdd43fb35fdf70ccaeceef4582a
zzangse1 <zzangse1014@gmail.com>
2024-05-24 19:12:34 +0900

    ✨ 뷰모델 생성

---
+++ app/src/main/java/com/zzangse/attendance_check/data/GroupViewModel.java
@@ -0,0 +1,24 @@
+package com.zzangse.attendance_check.data;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+public class GroupViewModel extends androidx.lifecycle.ViewModel {
+    private final MutableLiveData<String> groupName = new MutableLiveData<>();
+
+    public LiveData<String> getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String name) {
+        groupName.setValue(name);
+
+    }
+
+    @Override
+    public String toString() {
+        return "GroupViewModel{" +
+                "groupName=" + groupName +
+                '}';
+    }
+}
