935a07415aee10a813321b19d92c4dce062c3cff
sehoon <zzangse1014@gmail.com>
2024-06-22 17:46:09 +0900

    ✨ 자동저장클래스 생성

---
+++ app/src/main/java/com/zzangse/attendance_check/MySharedPreferences.java
@@ -0,0 +1,39 @@
+package com.zzangse.attendance_check;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class MySharedPreferences {
+    private static final String MY_ACCOUNT = "account";
+
+    public static void setUserId(Context context, String input) {
+        SharedPreferences prefs = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("MY_ID", input);
+        editor.apply(); // Use apply() instead of commit() for asynchronous saving
+    }
+
+    public static String getUserId(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE);
+        return prefs.getString("MY_ID", "");
+    }
+
+    public static void setUserPass(Context context, String input) {
+        SharedPreferences prefs = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("MY_PASS", input);
+        editor.apply(); // Use apply() instead of commit() for asynchronous saving
+    }
+
+    public static String getUserPass(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE);
+        return prefs.getString("MY_PASS", "");
+    }
+
+    public static void clearUser(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.clear();
+        editor.apply(); // Use apply() instead of commit() for asynchronous saving
+    }
+}
