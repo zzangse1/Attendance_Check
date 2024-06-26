3ac4024397525018b80a7f9ea68fd7af43a760a9
zzangse1 <zzangse1014@gmail.com>
2024-06-26 17:00:46 +0900

    🔥 java.sql.Date -> String형식으로 변경

--- app/src/main/java/com/zzangse/attendance_check/request/InsertCheckRequest.java
+++ app/src/main/java/com/zzangse/attendance_check/request/InsertCheckRequest.java
@@ -12,21 +12,15 @@ import java.util.Map;
 public class InsertCheckRequest extends StringRequest {
     private final static String URL = "http://zzangse.store/insert_check.php";
     private Map<String, String> map;
-    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-    public InsertCheckRequest(int priNum, String infoCheck, String infoDate/*java.sql.Date infoDate*/, Response.Listener<String> listener) {
+    public InsertCheckRequest(int priNum, String infoCheck, String infoDate, Response.Listener<String> listener) {
         super(Method.POST, URL, listener, null);
 
         map = new HashMap<>();
         map.put("priNum", String.valueOf(priNum));
         map.put("infoCheck", infoCheck);
         map.put("infoDate", infoDate);
-       // map.put("infoDate", simpleDateFormat.format(infoDate)); // Date를 문자열로 변환하여 저장
-
-        Log.d("InsertCheckRequest", "priNum: " + priNum);
-        Log.d("InsertCheckRequest", "infoCheck: " + infoCheck);
-       // Log.d("InsertCheckRequest", "infoDate: " + simpleDateFormat.format(infoDate));
-    }
+ }
 
     @Override
     protected Map<String, String> getParams() {
