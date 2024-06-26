7737d5dbff66ffd8a67f3451875f978202eb04d8
zzangse1 <zzangse1014@gmail.com>
2024-06-26 17:02:07 +0900

    ✨ 탭으로 프래그먼트 전환 기능 구현

--- app/src/main/java/com/zzangse/attendance_check/activity/FindAccountActivity.java
+++ app/src/main/java/com/zzangse/attendance_check/activity/FindAccountActivity.java
@@ -4,18 +4,29 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.tabs.TabLayout;
+import com.zzangse.attendance_check.R;
 import com.zzangse.attendance_check.databinding.ActivityFindAccountBinding;
+import com.zzangse.attendance_check.fragmentfind.FindIdFragment;
+import com.zzangse.attendance_check.fragmentfind.FindPwFragment;
 
 public class FindAccountActivity extends AppCompatActivity {
     private ActivityFindAccountBinding binding;
+    private FragmentManager fragmentManager;
+    private FindIdFragment findIdFragment;
+    private FindPwFragment findPwFragment;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initView();
         onClickBack();
-        onClickRadioBtn();
+        initFragment();
+        onClickTab();
     }
 
     private void initView() {
@@ -31,13 +42,41 @@ public class FindAccountActivity extends AppCompatActivity {
         });
     }
 
-    private void onClickRadioBtn() {
-        binding.rb1.setOnClickListener(v->{
-            binding.rb2.setChecked(false);
-        });
-        binding.rb2.setOnClickListener(v->{
-            binding.rb1.setChecked(true);
-        });
+    private void initFragment() {
+        fragmentManager = getSupportFragmentManager();
+        findIdFragment = new FindIdFragment();
+        findPwFragment = new FindPwFragment();
+        replace(findIdFragment);
+    }
 
+
+
+    private void replace(Fragment fragment) {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.find_fragment, fragment);
+        transaction.commit();
+    }
+
+    private void onClickTab() {
+        binding.tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() == 0) {
+                    replace(findIdFragment);
+                } else {
+                    replace(findPwFragment);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
