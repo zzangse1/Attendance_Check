eb236a7e53f18e352913b1e9711345567d06a454
zzangse1 <zzangse1014@gmail.com>
2024-08-02 17:31:02 +0900

    🎨 ID, PW 찾기 통합

--- app/src/main/java/com/zzangse/attendance_check/request/FindAccountSendRequest.java
+++ app/src/main/java/com/zzangse/attendance_check/request/FindAccountSendRequest.java
@@ -9,17 +9,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FindAccountSendRequest extends StringRequest {
-    private final static String URL = "http://zzangse.store/find_account/RandCode.php";
+    private final static String URL_PW_VER = "http://zzangse.store/find_account/CheckRandCodePW_VER.php";
+    private final static String URL_ID_VER = "http://zzangse.store/find_account/CheckRandCodeID_VER.php";
     private Map<String, String> map;
 
-    public FindAccountSendRequest(String userID, String randomCode, Response.Listener<String> listener) {
-        super(Method.POST, URL, listener, null);
-
-        map = new HashMap<>();
-        map.put("userID", userID);
-        map.put("randCode", randomCode);
-        Log.d("FIndAccount", userID + ", " + randomCode);
+    public enum RequestType {
+        PASSWORD_VERIFICATION,
+        ID_VERIFICATION
     }
+
+    public FindAccountSendRequest(RequestType requestType, String identifier, String userEmail, String randomCode, Response.Listener<String> listener) {
+        super(Method.POST, getUrl(requestType), listener, null);
+        map = new HashMap<>();
+        if (requestType == RequestType.PASSWORD_VERIFICATION) {
+            map.put("userID", identifier);
+            map.put("userEmail", userEmail);
+            map.put("randCode", randomCode);
+        } else if (requestType == RequestType.ID_VERIFICATION) {
+            map.put("userName", identifier);
+            map.put("userEmail", userEmail);
+            map.put("randCode", randomCode);
+        }
+    }
+
+    private static String getUrl(RequestType requestType) {
+        switch (requestType) {
+            case PASSWORD_VERIFICATION:
+                return URL_PW_VER;
+            case ID_VERIFICATION:
+                return URL_ID_VER;
+            default:
+                throw new IllegalArgumentException("Invalid request type");
+        }
+    }
+
 
     @Override
     protected Map<String, String> getParams() {
